88ce8c4435067cea39a544ca83c1a415ac76fa88
Shiyu_Huang <shiyu.huang@usp.ceu.es>
2026-04-22 04:54:12 +0000

    1

---
+++ ERRE/pruebas.r
@@ -0,0 +1,7 @@
+clase <- c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+as.numeric(clase)
+# Quitamos los elementos en las posiciones 1, 5 y 10
+sub_clase <- clase[c(1, 5, 10)]
+
+polla = c("20", "40")
+as.numeric(polla)
